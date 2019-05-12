38d443d878f4e2c55b8d04345f7b8aa6acdd3bf8
Sosnus <sosnus15@gmail.com>
2019-05-12 18:43:11 +0200

    Update configScript.sh

--- configScript.sh
+++ configScript.sh
@@ -1,78 +1,20 @@
-echo "RUN configScript.sh"
-mkdir workspace
-sudo apt-get update
-echo "INSTALL PYTHON"
-sudo apt install python-pip
-# sudo apt-get install python3.6
-# sudo apt install python3-pip    #python 3
-
-echo "INSTALL JUPYTER"
-pip install --upgrade pip3
-pip install jupyter
-
-sudo apt-get upgrade python3
-
+echo "#1 apt update ##################################################################"
 sudo apt update
 
-sudo apt install python3.7
-
-
-
-
-pip3 install --upgrade pip3
-
-python -V
-pip -V
-
-
-sudo apt update 
-
-sudo apt install software-properties-common 
-
-sudo add-apt-repository ppa:deadsnakes/ppa 
-
-sudo apt install python3.7 
-
-python3.7 --version 
-
-
-sudo apt-get upgrade python3
-
-sudo apt install python3-pip
-
-
-
-----
-sudo apt-get update
-sudo apt-get -y install python3-pip
-
-python -m pip install --upgrade pip
-
-sudo apt install jupyter-core
-
-pip install jupyterthemes
-
-# pip3 install --upgrade pip
-
-------
-
-sudo apt update
-## sudo apt install python3-pip
-
-
+echo "#2 install screenfetch ##################################################################"
 sudo apt install screenfetch
 
-
-pip3 --version
-
-
+echo "#3 reinstall and update python3 and pip ##################################################################"
 sudo python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall
 
-pip3 install jupyter
+echo "#4 install jupyter ##################################################################"
+sudo apt install jupyter-core
 
-## pip3 install jupyterthemes
-
+echo "#1 show version: screenfetch ******************************************************************"
 screenfetch
+echo "#2 show version: pip3 ******************************************************************"
 pip3 --version
+echo "#3 show version: python3 ******************************************************************"
 python3 -V
+echo "#4 show version: jupyter ******************************************************************"
 jupyter --version
