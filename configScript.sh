92c118f18ee3db0c591b370cf153b8f369b25bab
Sosnus <sosnus15@gmail.com>
2019-05-12 19:10:48 +0200

    Update configScript.sh

--- configScript.sh
+++ configScript.sh
@@ -8,7 +8,7 @@ echo "#3 reinstall and update python3 and pip ##################################
 sudo python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall
 
 echo "#4 install jupyter ##################################################################"
-sudo apt install jupyter-core
+sudo apt install jupyter-notebook
 
 echo "#1 show version: screenfetch ******************************************************************"
 screenfetch
@@ -16,5 +16,9 @@ echo "#2 show version: pip3 ****************************************************
 pip3 --version
 echo "#3 show version: python3 ******************************************************************"
 python3 -V
-echo "#4 show version: jupyter ******************************************************************"
+echo "#4 show version: jupyter core ******************************************************************"
 jupyter --version
+echo "#5 show version: jupyter notebook ******************************************************************"
+jupyter-notebook --version
+
+#TODO: notebook themes, jupyter lab
