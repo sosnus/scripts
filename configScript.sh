fbf6ae78f8809f370a9ce844b5e9345e6ab8554d
Sosnus <sosnus15@gmail.com>
2019-05-12 18:14:48 +0200

    Create configScript.sh

---
+++ configScript.sh
@@ -0,0 +1,78 @@
+echo "RUN configScript.sh"
+mkdir workspace
+sudo apt-get update
+echo "INSTALL PYTHON"
+sudo apt install python-pip
+# sudo apt-get install python3.6
+# sudo apt install python3-pip    #python 3
+
+echo "INSTALL JUPYTER"
+pip install --upgrade pip3
+pip install jupyter
+
+sudo apt-get upgrade python3
+
+sudo apt update
+
+sudo apt install python3.7
+
+
+
+
+pip3 install --upgrade pip3
+
+python -V
+pip -V
+
+
+sudo apt update 
+
+sudo apt install software-properties-common 
+
+sudo add-apt-repository ppa:deadsnakes/ppa 
+
+sudo apt install python3.7 
+
+python3.7 --version 
+
+
+sudo apt-get upgrade python3
+
+sudo apt install python3-pip
+
+
+
+----
+sudo apt-get update
+sudo apt-get -y install python3-pip
+
+python -m pip install --upgrade pip
+
+sudo apt install jupyter-core
+
+pip install jupyterthemes
+
+# pip3 install --upgrade pip
+
+------
+
+sudo apt update
+## sudo apt install python3-pip
+
+
+sudo apt install screenfetch
+
+
+pip3 --version
+
+
+sudo python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall
+
+pip3 install jupyter
+
+## pip3 install jupyterthemes
+
+screenfetch
+pip3 --version
+python3 -V
+jupyter --version
