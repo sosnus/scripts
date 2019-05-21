165792c8ac25f6629dfc4df979f3507923acabcf
Sosnus <sosnus15@gmail.com>
2019-05-21 03:49:14 +0200

    Create install-dotnet.sh

---
+++ install-dotnet.sh
@@ -0,0 +1,9 @@
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.2
+
+
+echo "KONIEC!!!!"
