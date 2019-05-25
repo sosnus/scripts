220b14ed6dd77b2572a333c5f61e84806823df73
Sosnus <sosnus15@gmail.com>
2019-05-25 15:21:55 +0200

    Create install-nodejs.sh

---
+++ install-nodejs.sh
@@ -0,0 +1,26 @@
+echo "install node, npm and azure functions"
+
+sudo apt install nodejs
+
+
+echo "install Azure shell"
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+sudo apt-get update
+sudo apt-get install curl apt-transport-https lsb-release gnupg
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor | \
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+    
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+    
+    
+    sudo apt-get update
+sudo apt-get install azure-cli
+
+
+az login
