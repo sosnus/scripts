7150d632c6a9825483f18c0f9f63c226c18e8034
Sosnus <sosnus15@gmail.com>
2019-06-13 23:22:05 +0200

    Create install-misc.sh

---
+++ install-misc.sh
@@ -0,0 +1,16 @@
+echo "INSTALL: FIGLET, SCREENFETCH, SL (train)
+
+
+
+sudo apt-get update -y
+sudo apt-get install -y figlet
+
+sudo apt update -y
+sudo apt install screenfetch -y
+
+sudo apt-get install sl -y
+
+sl
+
+
+figlet FIGLET WORKING
