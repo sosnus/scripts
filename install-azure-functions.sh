ff13b13440b8a9786441759840ec0e89940bf724
Sosnus <sosnus15@gmail.com>
2019-06-13 22:20:46 +0200

    Create install-azure-functions.sh

---
+++ install-azure-functions.sh
@@ -0,0 +1,8 @@
+echo("source: https://docs.microsoft.com/pl-pl/azure/azure-functions/functions-run-local")
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+
+sudo apt-get install azure-functions-core-tools
