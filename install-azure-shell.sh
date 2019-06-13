4f63c120751970b1d11868b384feb12c774815c2
Sosnus <sosnus15@gmail.com>
2019-06-13 23:01:11 +0200

    Create install-azure-shell.sh

---
+++ install-azure-shell.sh
@@ -0,0 +1,21 @@
+echo "install azure shell (az)"
+echo "source; https://docs.microsoft.com/bs-cyrl-ba/cli/azure/install-azure-cli-apt?view=azure-cli-latest"
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+sudo apt-get update
+sudo apt-get install curl apt-transport-https lsb-release gnupg
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor | \
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+    
+    
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+    
+    
+sudo apt-get update
+sudo apt-get install azure-cli
+
