d7765a0790068659ec8726daa17110d710c57018
Sosnus <sosnus15@gmail.com>
2019-06-18 23:31:47 +0200

    add command show IP addr

--- az/az-vm-sosnus-info.sh
+++ az/az-vm-sosnus-info.sh
@@ -1,3 +1,14 @@
-echo "show SOSNUS-VM info"
-az vm show -g sosnus-vm -n sosnus-vm
+echo "AZ SHOW SOSNUS-VM STATUS"
+# az vm get-instance-view --name sosnus-vm --resource-group sosnus-vm --query instanceView.statuses[1] --output json
+
+az vm get-instance-view --name sosnus-vm --resource-group sosnus-vm --query instanceView.statuses[1] --output table
+#  | jq -r '.displayStatus'
+
+echo "${b}"
+# echo $b
+# az vm get-instance-view --name sosnus-vm --resource-group sosnus-vm --query instanceView.statuses[1] --output table  
+# az vm show -g sosnus-vm -n sosnus-vm
 # ping 104.41.151.213
+
+echo "AZ get IP address..."
+az vm list-ip-addresses -n sosnus-vm -o table  
