f7f5d2af478ce89b912f8adc6e3805971265d74a
Sosnus <sosnus15@gmail.com>
2019-06-24 15:36:04 +0200

    Create rysownik.sh

---
+++ rysownik.sh
@@ -0,0 +1,12 @@
+echo("SOURCE: https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-18-04")
+
+sudo apt update
+
+sudo apt install nodejs
+
+sudo apt install npm
+
+nodejs -v
+
+
+npm install formidable
