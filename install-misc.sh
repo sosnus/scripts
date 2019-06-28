dd7dd70957b8aace5413644d4663347f697f88e4
Sosnus <sosnus15@gmail.comm>
2019-06-29 00:45:27 +0200

    add legacy folder and rm other files

--- install-misc.sh
+++ install-misc.sh
@@ -5,15 +5,15 @@ echo "INSTALL: FIGLET, SCREENFETCH, SL (train)"
 sudo apt-get update -y
 sudo apt-get install -y figlet
 
-sudo apt-get install git
+sudo apt-get install git -y
 
 
 git config --global user.email "sosnus15@gmail.comm"
 git config --global user.name "Sosnus"
 
 
+sudo apt install tmux -y
 
-sudo apt update -y
 sudo apt install screenfetch -y
 
 sudo apt-get install sl -y
