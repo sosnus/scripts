26440f9ebb794d6d70f574b395e90d2aad682588
Sosnus <sosnus15@gmail.com>
2019-07-16 15:29:11 +0200

    Create grove-pi.sh

---
+++ deb/grove-pi.sh
@@ -0,0 +1,3 @@
+# https://forum.dexterindustries.com/t/no-module-named-i2c-mutex/4703/4
+# or: curl -kL dexterindustries.com/update_tools | bash -s – --install-python-package --use-python3-exe-too --update-aptget --install-deb-deps
+sh -c "curl -kL dexterindustries.│ com/update_sensors | bash"
