67c55d7fb86a308b17fe1ecd0fa3e2d44f577c94
Sosnus <sosnus15@gmail.com>
2021-03-05 12:49:44 +0100

    Update images.sh

--- docker/images.sh
+++ docker/images.sh
@@ -34,7 +34,7 @@ docker run -d -it -v /data/dockervolumes/ros/:/data --restart=always --name ros-
 docker exec -it ros-test bash
 
 # JUPYTER LAB
-docker run -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -e GRANT_SUDO=yes --user root -v /Users/stapul/data/dockervolumes/jupyter:/home/jovyan/work --name jupyter-lab jupyter/datascience-notebook:latest start-notebook.sh --NotebookApp.token=''
+docker run -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -e GRANT_SUDO=yes --user root -v /Users/stanislawpulawski/data/dockervolumes/jupyter:/home/jovyan/work --name jupyter-lab jupyter/datascience-notebook:latest start-notebook.sh --NotebookApp.token=''
 
 # Jupyter lab skaner
 MyToken='MyTokenHere'
