b08d8cd03e9bb788ec1ddc963c41333f40a338da
Sosnus <sosnus15@gmail.com>
2022-10-19 17:23:26 +0200

    Create home-assistant-docker.sh

---
+++ home-assistant-docker.sh
@@ -0,0 +1,11 @@
+# docker HA
+docker run -d \
+  --name homeassistant \
+  --privileged \
+  --restart=unless-stopped \
+  -e TZ=MY_TIME_ZONE \
+  -v /PATH_TO_YOUR_CONFIG:/config \
+  --network=host \
+  ghcr.io/home-assistant/home-assistant:stable
+  
+  # https://www.home-assistant.io/getting-started/onboarding/
