cbf2b3468ad7eff17ef647679c5b2e92aff0f55d
Sosnus <sosnus15@gmail.com>
2023-05-31 22:57:45 +0200

    add some useful commands du and others

--- ubuntu/disk-storage-misc.sh
+++ ubuntu/disk-storage-misc.sh
@@ -2,3 +2,23 @@
 df -h
 # show size of files inside this and children folders
 du -h
+# more info (HUGE OUTPUT!)
+du -ah
+
+# THE BEST (add path after '/')
+du -cha --max-depth=1 / | grep -E "M|G"
+du -cha --max-depth=1 . | grep -E "M|G"
+# du -cha --max-depth=1 / | grep -E "M|G" from link below:
+https://askubuntu.com/questions/911865/no-more-disk-space-how-can-i-find-what-is-taking-up-the-space/911872#911872
+
+
+# rm journal files
+# https://awhost.pl/baza-wiedzy/usuwanie-logow-journald-z-var-log-journal/
+journalctl --disk-usage
+journalctl --vacuum-time=2d
+journalctl --vacuum-size=50M
+journalctl --rotate
+
+
+ls -lS | head -10
+du -sh * | sort -r
