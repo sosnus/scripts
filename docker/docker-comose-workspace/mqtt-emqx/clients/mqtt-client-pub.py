4947044720e4a6201c7e717165a08001891e9352
Sosnus <sosnus15@gmail.com>
2023-10-02 18:22:27 +0200

    add Heartbeat in thread

--- docker/docker-comose-workspace/mqtt-emqx/clients/mqtt-client-pub.py
+++ docker/docker-comose-workspace/mqtt-emqx/clients/mqtt-client-pub.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from threading import Thread
+import time
+
 import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     # This will be called once the client connects
@@ -8,6 +11,16 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(f"Message received [{msg.topic}]: {msg.payload}")
 
+
+
+def mqttHeartbeat():
+    print("mqtt connected, run mqttHeartbeat()")
+    while True:
+        print('Heartbeat')
+        client.publish("testtopic","Heartbeat") 
+        time.sleep(5)
+
+
 client = mqtt.Client("mqtt-test") # client ID "mqtt-test"
 client.on_connect = on_connect
 client.on_message = on_message
@@ -15,6 +28,7 @@ client.username_pw_set("user2", "user2")
 client.connect('127.0.0.1', 1883)
 ret= client.publish("testtopic","on") 
 print(ret)
+mqttHeartbeat()
 client.loop_forever()  # Start networking daemon
 
 # https://techoverflow.net/2021/12/27/how-to-set-username-password-in-paho-mqtt/
