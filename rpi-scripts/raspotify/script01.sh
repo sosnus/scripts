158677ea35bdc8017c4a2fb675e27e02b0c7301d
Sosnus <sosnus15@gmail.com>
2023-10-27 01:14:47 +0200

    raspotify

---
+++ rpi-scripts/raspotify/script01.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
+echo 'deb https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
