b745e61513682f7f7111135e483779c7a2a469d8
Sosnus <sosnus15@gmail.com>
2024-06-24 20:57:19 +0200

    add datetime formatting

--- docker/docker-compose-workspace/bash-compose/script.sh
+++ docker/docker-compose-workspace/bash-compose/script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+    # echo "Current time: $(date +'%Y-%m-%d %H:%M:%S %Z %z')"
 while true; do
-    echo send ping jetson-200
+    echo -n "[$(date +'%Y-%m-%d %H:%M:%S')]  "
+    echo -n "send ping jetson-200... "
     curl https://mws02-50458.wykr.es/api/push/UmpHntf4h4?status=up&msg=OK&ping=
+    echo " "
     sleep 60
+
 done
