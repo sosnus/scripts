a448128f7707d1aefa3dbb3dee9f8522527a146d
Sosnus <sosnus15@gmail.com>
2024-07-07 23:47:10 +0200

    add portainerio

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -3,3 +3,17 @@ sudo apt-get install ca-certificates curl gnupg lsb-release``
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+
+
+https://docs.portainer.io/start/install-ce/server/docker
+
+
+
+```bash
+docker volume create portainer_data
+
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+
+```
